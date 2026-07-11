5cb656805ed520c576d7ce2ba7e5773edbdc4d54
minog9959-png <minog9959@gmail.com>
2026-07-11 09:40:20 +0500

    Remove unused Imagetab import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// import Categories from "./components/Categories";
 import About from "./components/About";
 import Blog from "./components/Blog";
 import Hero from "./components/Hero";
@@ -8,7 +7,6 @@ import Team from "./components/Team";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import HeroSlider from "./components/HeroSlider";
-
 const App = () => {
   return (
     <>
