04f3cdfd3d809debff43d82fb0b0bc7c6286150f
minog9959-png <minog9959@gmail.com>
2026-07-11 09:19:35 +0500

    Remove unused import

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@
 import About from "./components/About";
 import Blog from "./components/Blog";
 import Hero from "./components/Hero";
-import Imagetab from "./components/imagetab";
 import Navbar from "./components/Navbar";
 import OurServices from "./components/OurServices";
 import Team from "./components/Team";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import HeroSlider from "./components/HeroSlider";
-// import Portfolio from "./components/Portfolio";
 
 const App = () => {
   return (
